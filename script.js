7746c0003c142b7de86d6bab9d1730bf030ebb60
Venkatbalaji M <venkatbalajim123@gmail.com>
2023-05-05 07:01:15 +0530

    My project commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function passwordShow() {
+    var x = document.getElementById("pass");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+function passwordCheck() {
+    var x = document.getElementById("pass").value;
+    if ((x.length < 8)||(!/[a-z]/.test(x))||(!/[A-Z]/.test(x))||(!/\d/.test(x))||(!/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/.test(x))) {
+        window.alert("Password does not satisfy the conditions.\nPassword must have the following :-\n1. Minimum length of 8 characters.\n2. At least one lowercase letter.\n3. At least one uppercase letter.\n4. At least one number.\n5. At least one punctuation symbol.");
+    } else {
+        window.alert("Account created successfully.\nPlease close this tab and login your account.");
+    }
+}
